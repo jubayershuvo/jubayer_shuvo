a6bab4c09e12f43fd525e7d822f7b52895ed9d75
Md Jubayer <mdjubayerislamshuvo34@gmail.com>
2024-08-30 21:42:14 +0600

    Update script.js

--- tools/video-downloader/youtube-downloader/script.js
+++ tools/video-downloader/youtube-downloader/script.js
@@ -18,7 +18,7 @@ const btn = document.getElementById('btn');
 
             const inputUrl = document.querySelector('#url');
             const url = inputUrl.value;
-            if(!url){
+            if(!url || !url.includes('youtube.com') || !url.includes('youtu.be')){
                 err.innerHTML = 'Input valid url';
                 form.classList.remove('hide');
                 load.classList.add('hide');
